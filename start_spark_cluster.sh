f0ed1cf4c553eae8d00e3071c5e183dd78265d8e
Micheal Quinn <quinnm@missouri.edu>
2015-09-29 08:23:28 -0500

    Updated by Shengyou

--- start_spark_cluster.sh
+++ start_spark_cluster.sh
@@ -23,6 +23,7 @@ fi
 
 MEM=$(scontrol show jobid $SLURM_JOB_ID | grep Memory | awk '{print $2}' | awk -F"=" '{print $2}')
 
+# start the master of the spark cluster
 ssh ${SSH_OPTS} ${nodes[0]} "module load $SPARK; nohup spark-class org.apache.spark.deploy.master.Master --ip ${nodes[0]} &> ${SCRATCH}/nohup-Master-${nodes[0]}-${SLURM_JOB_ID}-0.out &\echo -n Master: 0 ${nodes[0]} \$! >> $PID" &
 
 sleep 5
@@ -34,6 +35,7 @@ WEBUI=`egrep "MasterWebUI" ${SCRATCH}/nohup-Master-${nodes[0]}-${SLURM_JOB_ID}-0
 echo "  WebUI: $WEBUI" >> $PID
 
 
+# start the workers of the spark cluster
 for i in $( seq 0 $last )
 do
 #   echo Worker: ${nodes[$i]} >> $PID
