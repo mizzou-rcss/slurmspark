d798f88fce3b4809c0bc2b4dc2b26c623ba8af7b
Micheal Quinn <quinnm@missouri.edu>
2015-10-07 14:46:04 -0500

    cleanup

--- old/README.md
+++
@@ -1,7 +0,0 @@
-# SparkSlurm
-## Interactive Mode (using SBATCH)
-**Outline**
-* First, build out our working environment
-  * Define a master
-  * Define slaves
-
--- old/sparkscript.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext
-
-sc = SparkContext(appName="wordCount")
-file = sc.textFile("/home/shuang/test/spark2/moby-dick.txt")
-counts = file.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
-counts.saveAsTextFile("/home/shuang/test/spark2/wordcounts")
