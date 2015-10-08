b1b242fb6444b657181aaffe4b7fe862e0ab61d9
Micheal Quinn <quinnm@missouri.edu>
2015-10-08 11:06:48 -0500

    More comments

--- job_files/sbatch-srun-spark.sh
+++ job_files/sbatch-srun-spark.sh
@@ -3,18 +3,18 @@
 #
 #          FILE: sbatch-srun-spark.sh
 # 
-#         USAGE: ./sbatch-srun-spark.sh 
+#         USAGE: sbatch -k sbatch-srun-spark.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: SBATCH job file for starting a spark cluster in slurm
 # 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
+#       OPTIONS: 
+#  REQUIREMENTS: 
+#          BUGS: Please Report
+#         NOTES: 
 #        AUTHOR: Micheal Quinn (), quinnm@missouri.edu
-#  ORGANIZATION: 
+#  ORGANIZATION: RCSS
 #       CREATED: 10/06/2015 04:05:58 PM CDT
-#      REVISION:  ---
+#      REVISION: 0.1
 #===============================================================================
 #-------------------------------------------------------------------------------
 #  SBATCH CONFIG
@@ -25,15 +25,17 @@
 #SBATCH --job-name=testing
 #SBATCH --partition=Mem128
 #SBATCH --mem=60000
-
 #SBATCH --ntasks-per-node=1
 #SBATCH --exclusive
 #SBATCH --no-kill
 
-
 #-------------------------------------------------------------------------------
 #  CONFIG
 #-------------------------------------------------------------------------------
+## Location of slurmspark scripts
+##   Options: Valid path to a directory containing slurmspark scripts
+##     Notes: Should not need to change this unless you cloned this repo
+##            outside your home directory.
 SCRIPT_HOME="${HOME}/slurmspark/scripts"
 
 #-------------------------------------------------------------------------------
