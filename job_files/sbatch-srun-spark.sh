09e587dd06f8e631bc2ddff59327398f4df66fd6
Micheal Quinn <quinnm@missouri.edu>
2015-10-09 09:44:07 -0500

    Adding comments to job file

--- job_files/sbatch-srun-spark.sh
+++ job_files/sbatch-srun-spark.sh
@@ -19,12 +19,28 @@
 #-------------------------------------------------------------------------------
 #  SBATCH CONFIG
 #-------------------------------------------------------------------------------
+## The number of nodes for your Spark Cluster
 #SBATCH --nodes=3
+
+## Output file location.  This is where information on your cluster
+## Will be written.  If this path is not full, it will be relative to the
+## current working directory
 #SBATCH --output="%j.out"
+
+## How long the Spark cluster will last (in minutes)
 #SBATCH --time=128
+
+## The name for the job
 #SBATCH --job-name=testing
+
+## The Slurm partition to run on
 #SBATCH --partition=Mem128
+
+## How much memory to use for the job (in megabytes)
 #SBATCH --mem=60000
+
+## The following settings are in place for SlurmSpark to work.
+## Please do not change these unless you have a good reason
 #SBATCH --ntasks-per-node=1
 #SBATCH --exclusive
 #SBATCH --no-kill
@@ -41,9 +57,12 @@ SCRIPT_HOME="${HOME}/slurmspark/scripts"
 #-------------------------------------------------------------------------------
 #  EXPORT
 #-------------------------------------------------------------------------------
+## Export SCRIPT_HOME so it is available to the rest of the SlurmSpark
+## scripts.
 export SCRIPT_HOME=${SCRIPT_HOME}
 
 #-------------------------------------------------------------------------------
 #  SRUN
 #-------------------------------------------------------------------------------
+## Use srun to start the Spark Cluster
 srun --no-kill /bin/bash ${SCRIPT_HOME}/spark_run.sh
